8766fc7d8cefba029b1decab56b72d2d18d7c02a
Melissa Wilcox <melissa@emdubb.co>
2016-11-24 11:39:08 -0800

    set up database

--- challenge_api/challenge_api/settings.py
+++ challenge_api/challenge_api/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'users.apps.UsersConfig'
 ]
 
 MIDDLEWARE = [
@@ -75,8 +76,11 @@ WSGI_APPLICATION = 'challenge_api.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'challenge_api',
+        # 'NAME': os.path.join(BASE_DIR, 'db.mysql'),
+        'USER': 'root',
+        'PASSWORD': 'jmBq!x7a',
     }
 }
 
--- challenge_api/users/models.py
+++ challenge_api/users/models.py
@@ -2,4 +2,13 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+class User(models.Model):
+    created = models.DateTimeField(auto_now_add=True)
+    username = models.CharField(max_length=30)
+    email = models.EmailField()
+    fname = models.CharField(max_length=30)
+    lname = models.CharField(max_length=30)
+    profile_url = models.ImageField(upload_to=None, height_field=None, width_field=None)
+
+    class Meta:
+        ordering = ('created',)
