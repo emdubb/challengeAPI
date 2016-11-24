188c0579077d36e2da480bd9a8d4da0a12d82f92
Melissa Wilcox <melissa@emdubb.co>
2016-11-24 12:26:07 -0800

    add update and delete and get a single user

--- challenge_api/users/urls.py
+++ challenge_api/users/urls.py
@@ -3,5 +3,5 @@ from users import views
 
 urlpatterns = [
     url(r'^users/$', views.user_list),
-    # url(r'^users/(?P<pk>[0-9]+)/$', views.snippet_detail),
+    url(r'^users/(?P<pk>[0-9]+)/$', views.user_detail),
 ]
--- challenge_api/users/views.py
+++ challenge_api/users/views.py
@@ -15,7 +15,7 @@ class JSONResponse(HttpResponse):
 
 @csrf_exempt
 def user_list(request):
-    # List all code users, or create a new snippet.
+    # List all code users, or create a new user.
     if request.method == 'GET':
         users = User.objects.all()
         serializer = UserSerializer(users, many=True)
@@ -28,3 +28,27 @@ def user_list(request):
             serializer.save()
             return JSONResponse(serializer.data, status=201)
         return JSONResponse(serializer.errors, status=400)
+
+@csrf_exempt
+def user_detail(request, pk):
+    # Retrieve, update or delete a user.
+    try:
+        user = User.objects.get(pk=pk)
+    except user.DoesNotExist:
+        return HttpResponse(status=404)
+
+    if request.method == 'GET':
+        serializer = UserSerializer(user)
+        return JSONResponse(serializer.data)
+
+    elif request.method == 'PUT':
+        data = JSONParser().parse(request)
+        serializer = UserSerializer(user, data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return JSONResponse(serializer.data)
+        return JSONResponse(serializer.errors, status=400)
+
+    elif request.method == 'DELETE':
+        user.delete()
+        return HttpResponse(status=204)
