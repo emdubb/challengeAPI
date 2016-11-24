27b7ee3fd0e8290c7a3d9761dab2468dfa8057fa
Melissa Wilcox <melissa@emdubb.co>
2016-11-24 13:23:46 -0800

    switch to class based views

--- challenge_api/users/urls.py
+++ challenge_api/users/urls.py
@@ -3,8 +3,8 @@ from rest_framework.urlpatterns import format_suffix_patterns
 from users import views
 
 urlpatterns = [
-    url(r'^users/$', views.user_list),
-    url(r'^users/(?P<pk>[0-9]+)/$', views.user_detail),
+    url(r'^users/$', views.UserList.as_view()),
+    url(r'^users/(?P<pk>[0-9]+)/$', views.UserDetail.as_view()),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
--- challenge_api/users/views.py
+++ challenge_api/users/views.py
@@ -1,44 +1,49 @@
-from django.http import HttpResponse
-from rest_framework import status
-from rest_framework.decorators import api_view
-from rest_framework.response import Response
 from users.models import User
 from users.serializers import UserSerializer
+from django.http import Http404
+# from rest_framework.decorators import api_view
+from rest_framework import status
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
-@api_view(['GET', 'POST'])
-def user_list(request, format=None):
-    # List all code users, or create a new user.
-    if request.method == 'GET':
+class UserList(APIView):
+    # List all users
+    def get(self, request, format=None):
+    # if request.method == 'GET':
         users = User.objects.all()
         serializer = UserSerializer(users, many=True)
         return Response(serializer.data)
 
-    elif request.method == 'POST':
+    # Create a user
+    def post(self, request, format=None):
         serializer = UserSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-@api_view(['GET', 'PUT', 'DELETE'])
-def user_detail(request, pk, format=None):
-    # Retrieve, update or delete a user.
-    try:
-        user = User.objects.get(pk=pk)
-    except User.DoesNotExist:
-        return Response(status=status.HTTP_404_NOT_FOUND)
+class UserDetail(APIView):
+    # Retrieve, update or delete a user instance.
+    def get_object(self, pk):
+        try:
+            return User.objects.get(pk=pk)
+        except User.DoesNotExist:
+            raise Http404
 
-    if request.method == 'GET':
+    def get(self, request, pk, format=None):
+        user = self.get_object(pk)
         serializer = UserSerializer(user)
         return Response(serializer.data)
 
-    elif request.method == 'PUT':
+    def put(self, request, pk, format=None):
+        user = self.get_object(pk)
         serializer = UserSerializer(user, data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-    elif request.method == 'DELETE':
+    def delete(self, request, pk, format=None):
+        user = self.get_object(pk)
         user.delete()
         return HttpResponse(status=204)
