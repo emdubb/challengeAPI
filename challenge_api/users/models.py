fc624db58948ff7b72717977482e0eae3bb8d4c3
Melissa Wilcox <melissa@emdubb.co>
2016-11-24 13:09:50 -0800

    set blank to true for all user properties

--- challenge_api/users/models.py
+++ challenge_api/users/models.py
@@ -3,12 +3,12 @@ from __future__ import unicode_literals
 from django.db import models
 
 class User(models.Model):
-    created = models.DateTimeField(auto_now_add=True)
-    username = models.CharField(max_length=30)
-    email = models.EmailField()
-    fname = models.CharField(max_length=30)
-    lname = models.CharField(max_length=30)
-    profile_url = models.ImageField(upload_to=None, height_field=None, width_field=None)
+    created = models.DateTimeField(auto_now_add=True, blank=True)
+    username = models.CharField(max_length=30, blank=True)
+    email = models.EmailField(blank=True)
+    fname = models.CharField(max_length=30, blank=True)
+    lname = models.CharField(max_length=30, blank=True)
+    profile_url = models.ImageField(upload_to=None, height_field=None, width_field=None, blank=True)
 
     class Meta:
         ordering = ('created',)
